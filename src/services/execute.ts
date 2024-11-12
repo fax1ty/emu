a1bbdd1c9e970a512e29b523d6b398bc1f80a2e4
Artemiy Davydov <hi@fax1ty.dev>
2024-11-12 21:09:43 +0400

    feat: macos support

--- src/services/execute.ts
+++ src/services/execute.ts
@@ -1,13 +1,41 @@
+import { platform } from "@tauri-apps/plugin-os";
 import { Command, type SpawnOptions } from "@tauri-apps/plugin-shell";
 
+const getAndroidHomePrefix = () => {
+  const currentPlatfrom = platform();
+
+  switch (currentPlatfrom) {
+    case "windows":
+      return "%ANDROID_HOME";
+
+    default:
+      return "$ANDROID_HOME";
+  }
+};
+
+const makeCommand = (executable: string, ...args: string[]) => {
+  const currentPlatfrom = platform();
+
+  switch (currentPlatfrom) {
+    case "windows":
+      return Command.create("cmd.exe", [
+        "/c",
+        executable + ".exe " + args.join(" "),
+      ]);
+
+    default:
+      return Command.create("sh", ["-c", executable + " " + args.join(" ")]);
+  }
+};
+
 export const execute = async (
-  base: string,
-  args?: string | string[],
+  executable: string,
+  args: string[] = [],
   options?: SpawnOptions & { timeout?: number; verbose?: boolean }
 ) => {
-  const cmd = Command.create(base, args, options);
+  const cmd = makeCommand(executable, ...args);
   cmd.stdout.on("data", (v) => {
-    if (options?.verbose) console.info(base, args, v);
+    if (options?.verbose) console.info(executable, args, v);
   });
   if (typeof options?.timeout === "number") {
     setTimeout(() => {
@@ -16,5 +44,15 @@ export const execute = async (
   }
 
   const process = await cmd.execute();
-  return process.stdout;
+  return process.stdout.replace(/\r/gm, "");
+};
+
+export const emulator = (command: string) => {
+  const prefix = getAndroidHomePrefix();
+  return execute(`${prefix}/emulator/emulator`, [command]);
+};
+
+export const adb = (command: string) => {
+  const prefix = getAndroidHomePrefix();
+  return execute(`${prefix}/platform-tools/adb`, [command]);
 };
