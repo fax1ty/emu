bc4d20b0c67f20b396261c600f9378239af30a34
Davydov Artyom <hi@fax1ty.dev>
2024-11-14 07:42:12 +0400

    release: 0.2.0 (#4)
    * feat: tray-based design (#3)

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
 import { useEmulators } from "@/queries/emulator";
-import { Minus, X } from "@phosphor-icons/react";
-import { startEmulator } from "@/services/api/emulator";
-import { Item, Menu, useContextMenu } from "react-contexify";
-import { useState } from "react";
+import { Minus, Plus, X } from "@phosphor-icons/react";
+import { useMemo, useState } from "react";
 import { EmulatorListItem } from "@/components/emulator-list-item";
-import { AndroidHomeModal } from "@/components/modals/android-home";
 import { getCurrentWindow } from "@tauri-apps/api/window";
 import { useNoContext } from "@/hooks/no-context";
+import { AndroidHomeModal } from "@/components/modals/android-home";
+import { EmulatorOptionsModal } from "@/components/modals/emulator-options";
+import { cn } from "@/lib/utils";
+import { platform } from "@tauri-apps/plugin-os";
 
 import "./app.css";
 
@@ -15,15 +16,13 @@ function App() {
 
   const { data: emulators } = useEmulators();
 
-  const { show } = useContextMenu({
-    id: "emulator-more",
-  });
-
   const [selectedEmulator, setSelectedEmulator] = useState<string | null>(null);
 
-  const minimize = async () => {
+  const currentPlatfrom = useMemo(() => platform(), []);
+
+  const hide = async () => {
     const window = getCurrentWindow();
-    await window.minimize();
+    await window.hide();
   };
 
   const close = async () => {
@@ -31,75 +30,69 @@ function App() {
     await window.close();
   };
 
-  return (
-    <>
-      <nav
-        className="h-12 flex items-center justify-between pl-4"
-        data-tauri-drag-region
-      >
-        <p className="pointer-events-none">Device Manager</p>
+  const onEmulatorOptionsClose = () => {
+    setSelectedEmulator(null);
+  };
 
-        <ul className="flex">
-          <li>
-            <button
-              className="w-14 h-12 hover:bg-shark-700 duration-300 flex items-center justify-center"
-              onClick={minimize}
-            >
-              <Minus size={18} />
-            </button>
-          </li>
-          <li>
-            <button
-              className="w-14 h-12 hover:bg-shark-700 duration-300 flex items-center justify-center"
-              onClick={close}
-            >
-              <X size={18} />
-            </button>
-          </li>
-        </ul>
+  return (
+    <div
+      className={cn(
+        "w-full h-screen px-3 py-4 rounded-md overflow-hidden bg-black gap-4 flex flex-col",
+        currentPlatfrom === "windows" && "rounded-b-none",
+        currentPlatfrom === "macos" && "rounded-t-none"
+      )}
+    >
+      <nav className="bg-blue-300 h-7 flex items-center justify-between pl-2.5 rounded-md overflow-hidden">
+        <p className="pointer-events-none font-primary font-semibold text-xs text-blue-700">
+          Device Manager
+        </p>
+
+        <div className="flex text-blue-700 relative">
+          <button
+            className="w-7 h-7 hover:bg-blue-50 duration-300 flex items-center justify-center rounded-md rounded-r-none"
+            onClick={hide}
+          >
+            <Minus size={14} weight="bold" />
+          </button>
+          <button
+            className="w-7 h-7 hover:bg-blue-50 duration-300 flex items-center justify-center"
+            onClick={close}
+          >
+            <X size={14} weight="bold" />
+          </button>
+        </div>
       </nav>
 
-      <main className="flex-1 px-4 pb-2 py-1">
-        {emulators && (
-          <ul>
-            {Object.values(emulators).map((emulator, i) => (
+      <main className="flex flex-col flex-1">
+        <ul className="flex flex-1 flex-col gap-4">
+          {emulators &&
+            Object.values(emulators).map((emulator, i) => (
               <EmulatorListItem
                 key={i}
                 emulator={emulator}
-                onOptionsButtonPressed={(event) => {
-                  setSelectedEmulator(emulator.name);
-                  show({
-                    event,
-                  });
-                }}
+                onOptionsButtonPressed={() =>
+                  setSelectedEmulator(emulator.name)
+                }
               />
             ))}
-          </ul>
-        )}
+        </ul>
+
+        <button
+          title="Will be available in future versions"
+          disabled
+          className="flex h-10 bg-purple-800 justify-between items-center text-purple-200 font-primary font-semibold text-sm px-3 rounded hover:opacity-75 active:opacity-65 duration-300 disabled:opacity-60"
+        >
+          <span>Create</span>
+          <Plus size={18} weight="bold" />
+        </button>
       </main>
 
-      <Menu
-        id="emulator-more"
-        onVisibilityChange={(visible) => {
-          if (!visible) setSelectedEmulator(null);
-        }}
-      >
-        <Item
-          disabled={
-            !selectedEmulator ||
-            emulators?.[selectedEmulator]?.state !== "offline"
-          }
-          onClick={async () => {
-            if (!selectedEmulator) return;
-            await startEmulator(selectedEmulator, true);
-          }}
-        >
-          Cold Boot
-        </Item>
-      </Menu>
-
       <AndroidHomeModal />
-    </>
+      <EmulatorOptionsModal
+        name={selectedEmulator}
+        onClose={onEmulatorOptionsClose}
+      />
+    </div>
   );
 }
 
--- src/components/emulator-list-item.tsx
+++ src/components/emulator-list-item.tsx
@@ -5,11 +5,14 @@ import {
   EmulatorState,
   OnlineEmulator,
   OnlineEmulatorState,
+  OnlineEmulatorType,
 } from "@/types/emulator";
 import { isOnlineEmulator } from "@/types/guards/emulator";
 import {
-  Circle,
+  AndroidLogo,
   DeviceMobileCamera,
+  DeviceMobileSlash,
+  DeviceRotate,
   DotsThreeOutlineVertical,
   Play,
   Stop,
@@ -27,6 +30,22 @@ const getStatusAlt = (state: EmulatorState | OnlineEmulatorState) => {
     case "booting":
       return "Booting";
   }
+};
+
+const getTypeIcon = (
+  state: EmulatorState | OnlineEmulatorState,
+  type: OnlineEmulatorType | false
+) => {
+  if (state === "offline") return DeviceMobileSlash;
+  if (state === "booting") return DeviceRotate;
+  if (type === "watch") return Watch;
+  if (type === "tv") return TelevisionSimple;
+  return DeviceMobileCamera;
+};
+
+const getStateIcon = (state: EmulatorState | OnlineEmulatorState) => {
+  if (state === "offline") return Play;
+  return Stop;
 };
 
 interface EmulatorListItemProps {
@@ -48,49 +67,58 @@ export const EmulatorListItem = ({
     }
   };
 
+  const EmulatorTypeIcon = getTypeIcon(
+    emulator.state,
+    isOnlineEmulator(emulator) && emulator.type
+  );
+
+  const EmulatorStateIcon = getStateIcon(emulator.state);
+
   return (
-    <li className="flex items-center gap-2 h-12">
-      <Circle
-        size={12}
-        weight="fill"
+    <li className="flex items-center gap-3">
+      <div
         className={cn(
-          emulator.state === "offline" && "text-red-500",
-          emulator.state === "booting" && "text-yellow-500",
-          emulator.state === "online" && "text-green-500"
+          emulator.state === "offline" && "bg-red-400",
+          emulator.state === "booting" && "bg-amber-400",
+          emulator.state === "online" && "bg-teal-400",
+          "w-12 h-9 rounded-md flex items-center justify-center text-mauve-50"
         )}
-        alt={getStatusAlt(emulator.state)}
-      />
+        title={getStatusAlt(emulator.state)}
+      >
+        <EmulatorTypeIcon size={16} weight="fill" />
+      </div>
 
-      {isOnlineEmulator(emulator) && (
-        <>
-          {emulator.type === "tv" && (
-            <TelevisionSimple size={16} weight="fill" />
-          )}
-          {emulator.type === "watch" && <Watch size={16} weight="fill" />}
-          {emulator.type === "phone" && (
-            <DeviceMobileCamera size={16} weight="fill" />
-          )}
-        </>
-      )}
+      <div className="flex flex-col gap-1 flex-1 min-w-0 items-start">
+        <p className="text-xxs text-mauve-50 font-secondary font-medium truncate w-full">
+          {emulator.name}
+        </p>
 
-      <span className="flex-1">{emulator.name}</span>
+        {isOnlineEmulator(emulator) && (
+          <div className="bg-cyan-800 flex items-center rounded px-1 py-0.5 gap-1 text-cyan-200">
+            <AndroidLogo size={14} weight="fill" />
+            <p className="text-xxs font-primary font-semibold">
+              Android {emulator.version}
+            </p>
+          </div>
+        )}
+      </div>
 
-      <span className="flex gap-1.5">
+      <div className="flex gap-2.5">
         <button
+          className="px-4 bg-blue-900 text-blue-400 items-center flex rounded font-primary text-xs font-semibold h-8 gap-1.5 hover:opacity-75 active:opacity-65 duration-300"
           onClick={toggleEmulatorState}
-          className="hover:opacity-70 duration-300"
         >
-          {emulator.state === "offline" && <Play size={18} weight="fill" />}
-          {emulator.state !== "offline" && <Stop size={18} weight="fill" />}
+          <span>{emulator.state === "offline" ? "Run" : "Stop"}</span>
+          <EmulatorStateIcon size={14} weight="fill" />
         </button>
 
         <button
+          className="bg-blue-400 text-blue-900 items-center justify-center flex rounded h-8 w-8 hover:opacity-75 active:opacity-65 duration-300"
           onClick={onOptionsButtonPressed}
-          className="hover:opacity-80 duration-200"
         >
-          <DotsThreeOutlineVertical size={18} weight="fill" />
+          <DotsThreeOutlineVertical size={14} weight="fill" />
         </button>
-      </span>
+      </div>
     </li>
   );
 };
